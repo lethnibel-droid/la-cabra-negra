29281462d3bddc60d7f0b82e11ca12fab29218cf
lethnibel-droid <lethnibel@gmail.com>
2026-03-13 18:36:40 -0400

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// BUSCA ESTO:
+function login() {
+    auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+        .then(() => {
+            return auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+        })
+        .catch((error) => alert("Error: " + error.message));
+}
 
+// Y REEMPLÁZALO POR ESTO OTRO:
+function login() {
+    auth.setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+        .then(() => {
+            // Usamos Redirect en lugar de Popup para que no se bloquee
+            return auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+        })
+        .catch((error) => alert("Error al iniciar: " + error.message));
+}
